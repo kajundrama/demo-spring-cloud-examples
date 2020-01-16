ca902d09627c848e2ca8fabb5489196d7c8a234a
leejunhwi <kajundrama@gmail.com>
2020-01-16 22:24:32 +0900

    eureka 클러스터링 테스트 및 디버깅 로그 업로드

--- eureka-client/src/main/java/me/kajundrama/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/me/kajundrama/eurekaclient/EurekaClientApplication.java
@@ -3,11 +3,18 @@ package me.kajundrama.eurekaclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RestController
 public class EurekaClientApplication {
 
+  @GetMapping("/hello")
+  public String hello() {
+    return "hello";
+  }
   public static void main(String[] args) {
     SpringApplication.run(EurekaClientApplication.class, args);
   }
